bd79a724614fedff04cc4d9b177620b61706a139
Paladin-from-335 <andrei.anufriev.work@gmail.com>
2021-06-12 01:38:35 +0300

    initial commit

---
+++ demo/src/main/java/com/example/ratel/RatelApplicationRun.java
@@ -0,0 +1,13 @@
+package com.example.ratel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatelApplicationRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatelApplicationRun.class, args);
+    }
+
+}
