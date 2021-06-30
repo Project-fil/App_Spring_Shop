9ffad3fe3e147c126ce3fde115856379d24a7ccd
Anastasia Holovata <nastyagolovataya@gmail.com>
2021-06-30 14:06:41 +0300

    Added product functionality.

--- core/src/main/java/com/github/ratel/entity/Product.java
+++ core/src/main/java/com/github/ratel/entity/Product.java
@@ -1,6 +1,8 @@
 package com.github.ratel.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -14,53 +16,26 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "product_id", nullable = false, columnDefinition = "BIGINT", unique = true)
-    private long productId;
+    @Column(name = "id", nullable = false, columnDefinition = "BIGINT", unique = true)
+    private Long id;
 
-    @Column(name = "name", nullable = false, columnDefinition = "TEXT")
+    @Column(name = "vendor_code", nullable = false)
+    private String vendorCode;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "price", nullable = false, columnDefinition = "DECIMAL")
+    @Column(name = "price", nullable = false)
     private BigDecimal price;
 
-    @Column(name = "brand", nullable = false, columnDefinition = "TEXT")
-    private String brand;
-
-    @Column(name = "quantity", nullable = false, columnDefinition = "BIGINT")
+    @Column(name = "quantity", nullable = false)
     private long quantity;
 
-    @Column(name = "article", nullable = false, columnDefinition = "TEXT")
-    private String article;
-
-    @Column(name = "img", nullable = false, columnDefinition = "TEXT")
-    private String img;
-
-    @Column(name = "supplier", nullable = false, columnDefinition = "TEXT")
-    private String supplier;
-
-    @Column(name = "specification", nullable = false, columnDefinition = "TEXT")
-    private String specification;
-
-    @Column(name = "comments", columnDefinition = "TEXT")
-    private String comments;
-
-    public Product(
-            String name,
-            BigDecimal price,
-            String brand,
-            long quantity,
-            String article,
-            String img,
-            String supplier,
-            String specification
-    ) {
-        this.name = name;
-        this.price = price;
-        this.brand = brand;
-        this.quantity = quantity;
-        this.article = article;
-        this.img = img;
-        this.supplier = supplier;
-        this.specification = specification;
-    }
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EntityStatus status;
 }
